f0aaae42c785d6aa7912651710122de8675326bd
James Sinkler <jamessinkler713@gmail.com>
2021-03-23 10:55:32 -0700

    Adds Feeding: Displays Feeding

--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,24 @@ class Cat(models.Model):
 
 # garfield = Cat('Garfield', 'Tabby', 'I have never heard of Tabby', 9)
 # print(garfield)
+
+# tuples (value1, value2, value3) immutable pop() no changing of the values
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Feeding(models.Model):
+    date = models.DateField('when are we getting fed')
+    meal = models.CharField(
+        max_length=1,
+        # possible choices are the MEALS
+        choices = MEALS,
+        default = MEALS[0][0]
+    )
+    # make association to Cat Model, when cat is deleted, delete assoc. feedings
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
