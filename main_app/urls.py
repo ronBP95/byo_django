7bcf8b2f92a7298ce6021311f0298f3636fd281e
James Sinkler <jamessinkler713@gmail.com>
2021-03-22 20:53:30 -0700

    Model Form CRUD complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('cats/', views.cats_index, name='cats'),
-    path('cats/<int:cat_id>/', views.cats_show, name='cats_show')
+    path('cats/<int:cat_id>/', views.cats_show, name='cats_show'),
+    path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
 ]
